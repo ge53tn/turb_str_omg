c82b3fe638a6d83a36c35173a5f184d517d97501
ge53tn <nilsu.tuna@tum.de>
2025-03-09 09:58:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@ blockMesh > log.blockMesh 2>&1
 
 surfaceFeatureExtract > log.surfaceFeatureExtract 2>&1
 
-snappyHexMesh -overwrite > log.snappyHexMesh 2>&1
+decomposePar > log.decomposePar 2>&1  # Move it here, before snappyHexMesh
+
+mpirun -np 8 snappyHexMesh -overwrite -parallel > log.snappyHexMesh 2>&1  # Run in parallel
 
 rm -rf 0
 
 rm log.*
 
 cp -rf 0_orig 0
-
-decomposePar > log.decomposePar 2>&1
 
 mpirun -np 8 simpleFoam -parallel > log.simpleFoam 2>&1
 
@@ -23,4 +23,3 @@ reconstructPar > log.reconstructPar 2>&1
 pyFoamCopyLastToFirst.py . . > log.pyFoamCopyLastToFirst
 
 pyFoamClearCase.py . --processors-remove --keep-postprocessing > log.pyFoamClearCase
-
